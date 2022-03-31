0d25d1774f910d09740c5424b1d4aa2199dac818
iruchigupta <98231146+iruchigupta@users.noreply.github.com>
2022-03-31 21:45:01 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -6,11 +6,11 @@ document.querySelector(".dice .img1").setAttribute("src", image1);
 document.querySelector(".dice .img2").setAttribute("src", image2);
 
 if (randomNum1>randomNum2){
-  document.querySelector("h1").textContent= "🏆 Player 1 Wins!";
+  document.querySelector("h1").textContent= "Player 1 Wins!";
 }
 else if (randomNum1 === randomNum2){
   document.querySelector("h1").textContent= "It's a Draw!";
 }
 else{
-  document.querySelector("h1").textContent= "Player 2 Wins! 🏆";
+  document.querySelector("h1").textContent= "Player 2 Wins!";
 }
