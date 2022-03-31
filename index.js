f2bd997fd65d04440ac705cfd288e873a517a9c9
iruchigupta <98231146+iruchigupta@users.noreply.github.com>
2022-03-31 21:07:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNum1 = Math.floor(Math.random()*6) +1;
+var randomNum2 = Math.floor(Math.random()*6)+1;
+var image1 = "images/dice" + randomNum1 +".png";
+var image2 = "images/dice" + randomNum2 +".png";
+document.querySelector(".dice .img1").setAttribute("src", image1);
+document.querySelector(".dice .img2").setAttribute("src", image2);
+
+if (randomNum1>randomNum2){
+  document.querySelector("h1").textContent= "Player 1 Wins!";
+}
+else if (randomNum1 === randomNum2){
+  document.querySelector("h1").textContent= "It's a Draw!";
+}
+else{
+  document.querySelector("h1").textContent= "Player 2 Wins!";
+}
